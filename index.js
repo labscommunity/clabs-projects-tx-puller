532ccd98c4fc7a7eeadb6d03fe8a124d07dadd84
Marton Lederer <marton@lederer.hu>
2023-07-03 13:38:53 +0200

    feat: address counter

---
+++ index.js
@@ -0,0 +1,69 @@
+const { arGql } = require("ar-gql");
+const gql = arGql("https://ar-io.dev/graphql");
+
+(async () => {
+  let weeks = {};
+
+  const getWeekNum = (time) => {
+    const start = new Date(time.getFullYear(), 0, 1);
+
+    const days = Math.floor((time - start) / (24 * 60 * 60 * 1000));
+    const weekNumber = Math.ceil(days / 7);
+
+    return weekNumber;
+  }
+
+  const loop = async (cursor) => {
+    const res = await gql.run(`query {
+      transactions(
+        tags: [{ name: "Signing-Client", values: "ArConnect" }]
+        sort: HEIGHT_ASC
+        first: 100
+      ) {
+        edges {
+          node {
+            owner {
+              address
+            }
+            block {
+              timestamp
+            }
+          }
+          cursor
+        }
+        pageInfo {
+          hasNextPage
+        }
+      }
+    }`);
+
+    for (const { node } of res.data.transactions.edges) {
+      if (Object.values(weeks).find(val => val.includes(node.owner.address))) {
+        continue;
+      }
+
+      const time = new Date(node.block.timestamp * 1000)
+      let weekNumber = getWeekNum(time);
+
+      if (weekNumber === 0) {
+        time.setDate(time.getDate() - 7);
+        
+        weekNumber = getWeekNum(time.getTime()) + 1;
+      }
+        
+      if (!weeks[time.getFullYear() + "-" + weekNumber]) {
+        weeks[time.getFullYear() + "-" + weekNumber] = [node.owner.address];
+      } else {
+        weeks[time.getFullYear() + "-" + weekNumber].push(node.owner.address);
+      }
+    }
+
+    if (res.data.transactions.pageInfo.hasNextPage) {
+      await loop(res.data.transactions.edges[res.data.transactions.edges.length - 1].cursor);
+    }
+  };
+
+  await loop();
+
+  console.log(JSON.stringify(weeks, null, 2));
+})();
